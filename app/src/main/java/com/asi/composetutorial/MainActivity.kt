65dc75c2e85576bf89fca395546993fbbaf02dc4
史越勋 <yuexun.shi@jarvismedical.com>
2022-09-15 10:23:29 +0800

    [其他提交](develop): first

---
+++ app/src/main/java/com/asi/composetutorial/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.asi.composetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.asi.composetutorial.ui.theme.ComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+
+@Composable
+fun CustomButton(count: Int, onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text(text = count.toString())
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeTutorialTheme {
+        Greeting("Android")
+    }
+}
